c07dbb755b2da91f86b6d6bf11b9d9deef64d62a
maghdiel campos <maghdielcampos@gmail.com>
2023-10-08 00:28:37 -0600

    tempalte

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Aplicación</title>
+</head>
+<body>
+    <header>
+        <!-- Tu encabezado aquí -->
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <!-- Tu pie de página aquí -->
+    </footer>
+</body>
+</html>
