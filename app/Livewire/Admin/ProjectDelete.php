d0e667163fa400926c0a381287a927c0915d0ba2
maghdiel campos <maghdielcampos@gmail.com>
2023-10-10 12:20:39 -0600

    admin

---
+++ app/Livewire/Admin/ProjectDelete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class ProjectDelete extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.project-delete');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Admin\ProjectIndex;
+use App\Livewire\Admin\ProjectCreate;
+use App\Livewire\Admin\ProjectEdit;
+use App\Livewire\Admin\ProjectDelete;
+
 use App\Livewire\Proyecto\ProyectoIndex;
 use App\Livewire\Proyecto\ProyectoCreate;
 use App\Livewire\Proyecto\ProyectoEdit;
 
+Auth::routes();
 Route::get('/', ProyectoIndex::class)->name('proyecto.index');
 Route::get('/proyecto/create', ProyectoCreate::class)->name('proyecto.create');
 Route::get('/proyecto/{proyecto}/edit', ProyectoEdit::class)->name('proyecto.edit');
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/projects', ProjectIndex::class)->name('admin.projects.index');
+    Route::get('/admin/projects/create', ProjectCreate::class)->name('admin.projects.create');
+    Route::get('/admin/projects/{project}/edit', ProjectEdit::class)->name('admin.projects.edit');
+    Route::get('/admin/projects/{project}/delete', ProjectDelete::class)->name('admin.projects.delete');
+});
