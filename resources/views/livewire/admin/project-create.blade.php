6a1144d85df0ab44819fa1e05ce84d0e95c6ba0f
maghdiel campos <maghdielcampos@gmail.com>
2023-10-10 19:26:45 -0600

    dsahboard

--- resources/views/livewire/admin/project-create.blade.php
+++ resources/views/livewire/admin/project-create.blade.php
@@ -1,3 +1,30 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Crear un nuevo proyecto') }}</div>
+                    <form wire:submit.prevent="createProject" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="titulo">Título</label>
+                            <input type="text" wire:model="titulo" class="form-control" id="titulo"
+                                   placeholder="Título del proyecto">
+                        </div>
+                        <div class="form-group">
+                            <label for="descripcion">Descripción</label>
+                            <textarea wire:model="descripcion" class="form-control" id="descripcion"
+                                      placeholder="Descripción del proyecto"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="imagen">Imagen</label>
+                            <input type="file" wire:model="imagen" class="form-control-file" id="imagen">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Guardar Proyecto</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ use App\Livewire\Proyecto\ProyectoCreate;
 use App\Livewire\Proyecto\ProyectoEdit;
 
 Route::get('/', ProyectoIndex::class)->name('proyecto.index');
-Route::get('/proyecto/create', ProyectoCreate::class)->name('proyecto.create');
-Route::get('/proyecto/{proyecto}/edit', ProyectoEdit::class)->name('proyecto.edit');
 
 Auth::routes();
 
